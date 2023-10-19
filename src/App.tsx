16a20305bb7b75028858e43b4641f3a4e0b96bea
JBryskiewicz <kuba@fiku.eu>
2023-10-19 16:45:08 +0200

    switch added to change pages of input form in cv creator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useSelector } from "react-redux";
 import Information from "./components/forms/informationForm";
 import Header from "./components/header";
 import Summary from "./components/summary/summary";
+import { RootState } from "./redux/store";
 
 const App = () => {
+  const { step } = useSelector((state: RootState) => state.information);
+
   return (
     <>
       <Header />
       <main className="hero">
-        <Information />
+        {(() => {
+          switch (step) {
+            case 1:
+              return <Information />;
+            case 2:
+              return <div className="section" />;
+            default:
+              return <Information />;
+          }
+        })()}
         <Summary />
       </main>
     </>
